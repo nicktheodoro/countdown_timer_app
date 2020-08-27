23ad641845659d00dd00fbf5202927fe044c6e89
Nicolas Theodoro <nicolastheodoro@gmail.com>
2020-08-27 15:28:21 -0300

    minor changes to refine code

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-document.getElementById("timeTo").addEventListener("input", calcDateIntevarl)
+document.querySelector("#timeToDate").addEventListener("input", calcDateIntevarl)
 
 function calcDateIntevarl() {
+    let timeToDate = document.querySelector('#timeToDate')
+    timeToDate = Date.parse(timeToDate.value)
 
-    let timeTo = document.querySelector('#timeTo')
-    timeTo = Date.parse(timeTo.value)
+    const dateNow = Date.now()
 
-    const now = Date.now()
+    let interval = timeToDate - dateNow
+    convertToMiliseconds(interval)
 
-    let interval = timeTo - now
-    convertMiliseconds(interval)
-
-    // select element
     document.querySelector('.clock-day').innerHTML = days
     document.querySelector('.clock-hours').innerHTML = hours + 3 // add 3 hours to brazil utc time
     document.querySelector('.clock-minutes').innerHTML = minutes
@@ -19,19 +17,18 @@ function calcDateIntevarl() {
     // start dynamic counter of date
     startTimer = setInterval(calcDateIntevarl, 1000)
 
-    if (now >= timeTo) {
-            clearInterval(startTimer);
-			document.querySelector('.clock-day').innerHTML = 'D'
-			document.querySelector('.clock-hours').innerHTML = 'O'
-			document.querySelector('.clock-minutes').innerHTML = 'N'
-			document.querySelector('.clock-seconds').innerHTML = 'E'
+    if (dateNow >= timeToDate) {
+        clearInterval(startTimer);
+		document.querySelector('.clock-day').innerHTML = 'D'
+		document.querySelector('.clock-hours').innerHTML = 'O'
+		document.querySelector('.clock-minutes').innerHTML = 'N'
+		document.querySelector('.clock-seconds').innerHTML = 'E'
 	}
 }
 
-function convertMiliseconds(ms) {
-
-    days     = Math.floor(ms / 86400000)
-    seconds  = Math.floor(( ms / 1000 ) % 60)
-    minutes  = Math.floor(( ms / 60000 ) % 60)
-    hours    = Math.floor(( ms / 3600000 ) % 24)
+function convertToMiliseconds(ms) {
+    days    = Math.floor(ms / 86400000)
+    seconds = Math.floor(( ms / 1000 ) % 60)
+    minutes = Math.floor(( ms / 60000 ) % 60)
+    hours   = Math.floor(( ms / 3600000 ) % 24)
 }
